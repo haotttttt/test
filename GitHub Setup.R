f023fa655c35b4ac527395ce984874d3f9c96366
Tong Hao <hhtttt1103@gmail.com>
2025-10-31 10:26:13 -0500

    Small revision prior to pushing to github

---
+++ GitHub Setup.R
@@ -0,0 +1,11 @@
+library(usethis)
+
+# First register for a github account.
+create_github_token()
+gitcreds::gitcreds_set()
+
+
+# To push a git repo to GitHub:
+use_github()
+
+# I'm going to start a new project first.
